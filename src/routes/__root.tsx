84518f7adf1be740dac5ab25ae5a69df26f1d723
Victor <victor1@eximbay.com>
2026-05-27 17:44:07 +0900

    chore: remove DebugOverlay from root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,5 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { DebugOverlay } from '@/components/debug-overlay'
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      <DebugOverlay />
-    </>
-  ),
+  component: () => <Outlet />,
 })
