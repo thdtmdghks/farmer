ddcc5b1d51ff038961ae2fc762a4f7425d94e50f
Victor <victor1@eximbay.com>
2026-07-01 01:44:04 +0900

    refactor(routes): /resume 요약본, /resume-all 통합본으로 라우트 재구성

--- src/routes/resume-all.tsx
+++ src/routes/resume-all.tsx
@@ -1,14 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Resume2Summary } from '@/features/resume/components/resume2-summary'
+import { Resume2Details } from '@/features/resume/components/resume2-details'
 
-export const Route = createFileRoute('/resume-summary')({
-  component: ResumeSummaryPage,
+export const Route = createFileRoute('/resume-all')({
+  component: ResumeAllPage,
 })
 
-function ResumeSummaryPage() {
+function ResumeAllPage() {
   return (
     <div className="mx-auto max-w-3xl bg-white p-8 print:p-6">
       <Resume2Summary />
+      <Resume2Details />
     </div>
   )
 }
--- src/routes/resume.tsx
+++ src/routes/resume.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Resume2Summary } from '@/features/resume/components/resume2-summary'
-import { Resume2Details } from '@/features/resume/components/resume2-details'
 
 export const Route = createFileRoute('/resume')({
   component: ResumePage,
@@ -10,7 +9,6 @@ function ResumePage() {
   return (
     <div className="mx-auto max-w-3xl bg-white p-8 print:p-6">
       <Resume2Summary />
-      <Resume2Details />
     </div>
   )
 }
