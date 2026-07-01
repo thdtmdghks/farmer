fab787f877fe626154a1e55abbb6f7fa75e81334
Victor <victor1@eximbay.com>
2026-07-01 11:41:04 +0900

    refactor: rename features/features to features/highlights

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Navbar } from '@/features/navigation/components/navbar'
 import { HeroSection } from '@/features/hero/components/hero-section'
-import { FeaturesSection } from '@/features/features/components/features-section'
+import { FeaturesSection } from '@/features/highlights/components/features-section'
 import { ShowcaseSection } from '@/features/showcase/components/showcase-section'
 import { StatsSection } from '@/features/stats/components/stats-section'
 import { CtaSection } from '@/features/cta/components/cta-section'
